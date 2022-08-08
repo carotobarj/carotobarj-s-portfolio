dd6e365896988273922964dd163011da06621e81
Caro Tobar J <carotobarj@gmail.com>
2022-08-07 22:26:45 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Header from "./components/Header/Header";
-import Home from "./components/home/home";
+import Home from "./components/Home/Home";
 import SkillsDescription from "./components/Skills/SkillsDescription/SkillsDescription";
-import Contact from "./components/contact/contact";
-import Navbar from "./components/nav/nav";
+import Contact from "./components/Contact/Contact";
+import Navbar from "./components/Nav/Nav";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
 
