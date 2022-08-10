c1097b18d744d28b3985167e9ecb0085cd19131e
Caro Tobar J <carotobarj@gmail.com>
2022-08-10 00:51:59 -0500

    actualizacion responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,10 +7,10 @@ function About() {
         <h2>
         <span>About Me</span> <br />
         <p>Hello! 👋🏻 I am Carolina, I initially studied business administration and marketing.</p>
-        <p>I am a person who seeks professional and personal growth, focused on teamwork, last year I made the decision to reinvent myself professionally so I started training in an area that I have always been passionate about; programming.</p>
+        <p>I am a person who seeks professional and personal growth, focused on teamwork.</p>
         <p>Being a creative person I have focused on the frontend and UX/UI area, because I firmly believe that solutions can be solved efficiently where design, user experience and functionality are important parts in the development of any web application.</p>
         <p>
-          I consider that my background in marketing and business administration allows me to bring to organizations a comprehensive knowledge in terms of production processes, as well as experience in the area of customer service both internal and external, business creation, project development, management of students in educational environments, management of suppliers, good interpersonal relationships.</p>
+          I consider that my background in marketing and business administration allows me to bring to organizations a comprehensive knowledge in terms of production processes, as well as experience in the area of customer service both internal and external, business creation, project development, management, good interpersonal relationships.</p>
       </h2>
 
     </div>
