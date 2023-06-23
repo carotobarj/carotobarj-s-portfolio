abb20332ef0312bb41c506502b64622059914634
Caro Tobar J <82724532+carotobarj@users.noreply.github.com>
2023-06-23 08:48:04 -0500

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,7 +6,7 @@ function Buttons() {
       <a href="https://drive.google.com/file/d/1uZWf_hnqFrG0gbOkvmx4mXU2mC21R1RN/view?usp=sharing" className="btn pri" target={"blank"}>
         ENGLISH CV
       </a>
-      <a href="https://drive.google.com/file/d/1KarmZIoKTPtsh_PjPNzbOi7GLjhn71gd/view" className="btn sec" target={"blank"}>
+      <a href="https://drive.google.com/file/d/1B6wEvU5zSvVJ5WvsUBOUWPFVFFao6aKS/view" className="btn sec" target={"blank"}>
       SPANISH CV
       </a>
     </div>
