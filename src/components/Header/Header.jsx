f926cc6bbf3dbf768fbd90d6a7e781430f1b6283
Caro Tobar J <82724532+carotobarj@users.noreply.github.com>
2023-06-23 08:39:24 -0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div id='home' className='container header-container'>
       <h3> I'm Carolina Tobar Jaramillo</h3>
-      <h2>Front-End Web Developer</h2>
+      <h2>UX / UI Developer</h2>
     </div>
   );
 }
