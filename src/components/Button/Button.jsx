38fc10c0b6c9950295bc0219793f57342f3c15dc
Caro Tobar J <82724532+carotobarj@users.noreply.github.com>
2023-12-16 01:28:45 -0500

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,10 +3,10 @@ import "./Button.css";
 function Buttons() {
   return (
     <div className="container button-container">
-      <a href="https://drive.google.com/file/d/1vb63p2XJG3lqHBEIEtMNCDhYClRv3vnp/view?usp=sharing" className="btn pri" target={"blank"}>
+      <a href="https://drive.google.com/file/d/1a1ZGdRf_AeU44aF5C5JXbczJljT5eunH/view?usp=sharing" className="btn pri" target={"blank"}>
         ENGLISH CV
       </a>
-      <a href="https://drive.google.com/file/d/1sBahavt0x7QHoVVg87izw4fFruyRs7Uw/view?usp=sharing" className="btn sec" target={"blank"}>
+      <a href="https://drive.google.com/file/d/1oDan5VCRih4rSFhIos8vGlaE7Du-H6Ut/view?usp=sharing" className="btn sec" target={"blank"}>
       SPANISH CV
       </a>
     </div>
