9406d088f98b1477c8e1152f53ae92525a229f9d
Caro Tobar J <82724532+carotobarj@users.noreply.github.com>
2023-12-15 20:31:04 -0500

    Update Button.jsx
    actualización links cv

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,10 +3,10 @@ import "./Button.css";
 function Buttons() {
   return (
     <div className="container button-container">
-      <a href="https://drive.google.com/file/d/1uZWf_hnqFrG0gbOkvmx4mXU2mC21R1RN/view?usp=sharing" className="btn pri" target={"blank"}>
+      <a href="https://drive.google.com/file/d/1vb63p2XJG3lqHBEIEtMNCDhYClRv3vnp/view?usp=sharing" className="btn pri" target={"blank"}>
         ENGLISH CV
       </a>
-      <a href="https://drive.google.com/file/d/1B6wEvU5zSvVJ5WvsUBOUWPFVFFao6aKS/view" className="btn sec" target={"blank"}>
+      <a href="https://drive.google.com/file/d/1sBahavt0x7QHoVVg87izw4fFruyRs7Uw/view?usp=sharing" className="btn sec" target={"blank"}>
       SPANISH CV
       </a>
     </div>
